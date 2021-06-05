82c30bcbe9a082c8f730a9fd98d8c7e638c21063
Leo Liu <leoliu8@deloitte.com>
2021-06-04 20:58:34 -0400

    added support for funcs

---
+++ SwiftWheel/Sources/Source/Func.swift
@@ -0,0 +1,67 @@
+// accessLevel func name(parameter1, parameter2, ...): returnType
+public struct FuncType {
+	public let rawValue: String
+    public let accessLevel: AccessLevel
+    public let name: String
+    public let parameters: [ParameterType]
+    public let doesThrow: Bool
+    public let returnTypeName: String?
+
+	public init(string: String) throws {
+		let stringUtility = StringUtility()
+
+		self.rawValue = string
+		// e.g. `override private func funcName_1(for p1: @escape [Array<Any>]? = nil) throws -> (key: Int?, value: String)`
+		let pattern = #"([\w\s]*)?\s+func\s+(\w+)\(([\w\s\:\.\(\)\?\=\,\[\]\<\>\@]*)\)\s?(throws)?\s?\-?\>?\s?([\w\s\[\]\(\)\<\>\:\,\?]*)?(\?)?\s?\{"#
+		let captured = stringUtility.captured(string, pattern: pattern)
+		//print("--- ||||\(string)\n||||\(captured)")
+
+		guard captured.count == 6 else {
+			throw SourceError.generic(message: "Func should capture 6 elements: \(captured), '\(string)'")
+		}
+
+		guard !captured[0].contains("static"), !captured[0].contains("class") else {
+			throw SourceError.generic(message: "Does not suppoort static / class func now: \(captured[0])")
+		}
+
+		// Access level
+		if let first = stringUtility.firstOccurance(captured[0], candidates: AccessLevel.allCases.map { $0.rawValue }), 
+			let accessLevel = AccessLevel(rawValue: first) {
+			self.accessLevel = accessLevel
+		} else {
+			accessLevel = .internal
+		}
+	
+		self.name = captured[1]
+		self.parameters = try ParameterType.matched(from: captured[2])
+		self.doesThrow = !captured[3].isEmpty
+		self.returnTypeName = captured[4].isEmpty ? nil : captured[4]
+		//self.returnTypeIsOptional = !captured[5].isEmpty
+	}
+
+	public static func matched(from string: String) throws -> [FuncType] {
+		let stringUtility = StringUtility()
+
+		var content = string
+		// Remove comments
+		for type in CommentType.allCases {
+			content = try type.stripped(from: content)
+		}
+
+		return stringUtility.findLines(content, pattern: #"\s+func\s+\w+\("#).compactMap { try? FuncType(string: $0) }
+	}
+}
+
+extension FuncType: CustomStringConvertible {
+	public var description: String {
+		"""
+		---- Func name: \(name)
+		AccessLevel: \(accessLevel)
+		Parameters: \(parameters)
+		Throws: \(doesThrow)
+		Return type name: \(returnTypeName ?? "N/A")
+		\(rawValue)
+		---- End of func \(name)
+		"""	
+	}
+}
---
+++ SwiftWheel/Sources/Source/KnownClasses.swift
@@ -0,0 +1,67 @@
+public enum KnownClasses: String, CaseIterable {
+	case int = "Int"
+	case bool = "Bool"
+	case string = "String"
+	case date = "Date"
+
+	case nsCoder = "NSCoder"
+	case cgRect = "CGRect"
+
+	// TODO: process array and dictionary differently
+	case intArray = "[Int]"
+	case stringArray = "[String]"
+	case stringAnyObjectDictionary = "[String: AnyObject]"
+
+	public var defaultValue: String {
+		switch self {
+		case .int: return "0"
+		case .bool: return "true"
+		case .string: return "\"\""
+		case .date: return "Date()"
+
+		case .nsCoder: return "NSCoder()"
+		case .cgRect: return "CGRect()"
+
+		case .intArray: return "[0]"
+		case .stringArray: return "[\"\"]"
+		case .stringAnyObjectDictionary: return #"[String: AnyObject]()"#
+		}
+	}
+
+	/// Returns `nil` if it's not a known class
+	public static func defaultValue(typeName string: String, isOptional: Bool = false) -> String? {
+		if isOptional {
+			return "nil"
+		}
+
+		if let theClass = KnownClasses(rawValue: string) {
+			return theClass.defaultValue
+		}
+
+		// UICollectionView must be initialized with a non-nil layout parameter
+		// In UIViewController, views are likely to be `nil`
+		if [
+			"UIViewControllerTransitionCoordinator", 
+			"NSManagedObjectContext",
+			"NSManagedObject",
+			"UICollectionView",
+			//"UIViewController",
+			//"UITableViewCell",
+			//"UIView",
+		].contains(where: string.contains) {
+			return nil
+		}
+
+		// Enums like UIImagePickerController.SourceType are not supported
+		guard !string.contains(".") else {
+			return nil
+		}
+
+		let stringUtility = StringUtility()
+		if stringUtility.matches(string, pattern: "^(NS|UI|CG).*") {
+			return string.trimmingCharacters(in: .whitespacesAndNewlines) + "()"
+		}
+
+		return nil
+	}
+}
